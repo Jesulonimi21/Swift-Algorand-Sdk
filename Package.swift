a15401935781e945c3b490fc58fb9e61266658e8
yuliani <inailuy@me.cm>
2021-12-23 16:41:28 -0500

    init commit submission working

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(url:  "https://github.com/vzsg/ed25519.git", .upToNextMajor(from: "0.1.0"))
 //        .package(url: "https://github.com/AndrewBarba/ed25519.git", .upToNextMajor(from: "1.1.0")),
-        .package(name:"Ed25519",url: "https://github.com/AndrewBarba/ed25519.git",  from: "1.0.0"),
+        .package(name:"Ed25519",url: "https://github.com/AndrewBarba/ed25519.git",  from: "1.1.0"),
 //        .package(name:"swift-crypto", url: "https://github.com/apple/swift-crypto.git", .branch("main")),
         .package(
             url: "https://github.com/Flight-School/MessagePack",
