618a93a96306949f4d0d9a6294722781ce63bdd7
jesulonimi21 <anjola4jeez@gmail.com>
2022-02-19 13:43:09 +0100

    Added Account and App transaction examples

---
+++ Sources/swift-algorand-sdk/examples/AssetOpp.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jesulonimi Akingbesote on 18/02/2022.
+//
+
+import Foundation
+// Create Asset
+// Change Manager
+// Opt In
+// Send Asset
+// ClawbackAsset
+// Freeze Asset
---
+++ Sources/swift-algorand-sdk/examples/IndexerOpp.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jesulonimi Akingbesote on 18/02/2022.
+//
+
+import Foundation
