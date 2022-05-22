c3a1fc2d3412f14e45b3178b468ba3ad784c2807
jesulonimi21 <anjola4jeez@gmail.com>
2022-05-22 11:08:06 +0100

    Add: contract functions

--- Sources/swift-algorand-sdk/abi/Contract.swift
+++ Sources/swift-algorand-sdk/abi/Contract.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-class Contract{
+@available(macOS 10.13, *)
+public struct Contract{
     
+    public var name: String
+    public var desc: String
+    public var network: [String: NetworkInfo]
+    public var methods: [Method];
+    
+    
+    init(name: String, desc: String, methods: [Method], network: [String: NetworkInfo]) {
+        self.name = name;
+        self.methods = methods
+        self.desc = desc
+        self.network = network
+    }
+
+    public struct NetworkInfo {
+        public var appId: Int64
+        init(appId: Int64) {
+            self.appId = appId;
+        }
+        
+    }
 }
